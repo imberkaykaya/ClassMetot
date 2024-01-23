23c0f5ddbb5dbd6882c551e334a3010aaf0ba993
Berkay Kaya <80210343+imberkaykaya@users.noreply.github.com>
2024-01-24 00:53:33 +0300

    Info updated

--- ClassMetot/ClassMetot/Program.cs
+++ ClassMetot/ClassMetot/Program.cs
@@ -4,19 +4,19 @@ class Program
     static void Main(string[] args)
     {
         Musteri musteri1 = new Musteri();
-        musteri1.Ad = "Muhittin";
-        musteri1.Soyad = "Karaca";
-        musteri1.Sehir = "İzmir";
+        musteri1.Ad = "Aziz";
+        musteri1.Soyad = "Sancar";
+        musteri1.Sehir = "Mardin";
 
         Musteri musteri2 = new Musteri();
         musteri2.Ad = "Neşet";
         musteri2.Soyad = "Ertaş";
-        musteri2.Sehir = "Niğde";
+        musteri2.Sehir = "Kırşehir";
 
         Musteri musteri3 = new Musteri();
         musteri3.Ad = "Oğuz";
         musteri3.Soyad = "Atay";
-        musteri3.Sehir = "LA";
+        musteri3.Sehir = "Kastamonu";
 
         Musteri[] musteriEkle = new Musteri[] { musteri1, musteri2};
 
